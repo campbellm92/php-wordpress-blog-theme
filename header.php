2d8b948fa3488c39db4eda02bda5cb55e7f4cc21
Matthew Campbell <m.campbell92@gmail.com>
2025-03-11 15:17:58 +0100

    initial header setup

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- title generated from method in functions.php -->
+    <?php
+    wp_head();
+    ?>
+</head>
+
+<body>
+    <header>
+        <h1>Wren</h1>
+        <nav>
+            <a href="">LINK 1</a>
+            <a href="">LINK 2</a>
+            <a href="">LINK 3</a>
+        </nav>
+
+
+    </header>
