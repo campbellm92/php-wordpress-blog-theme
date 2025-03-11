8436894150c1b5821e24f30fe769f043f2c12080
Matthew Campbell <m.campbell92@gmail.com>
2025-03-11 18:28:49 +0100

    render posts w php snippet

--- front-page.php
+++ front-page.php
@@ -4,6 +4,26 @@ get_header();
 
 <!-- main content here -->
 
+
+<main>
+    <div class="content-wrapper">
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+        }
+
+        ?>
+
+
+</main>
+</div>
+
+
+</div>
+
 <?php
 get_footer();
 ?>
