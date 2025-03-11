e812f7cbf1671944fa48ca4b17e3ba478fbf5329
Matthew Campbell <m.campbell92@gmail.com>
2025-03-11 18:29:19 +0100

    render menu dynamically

--- header.php
+++ header.php
@@ -5,19 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- title generated from method in functions.php -->
+
     <?php
     wp_head();
     ?>
 </head>
 
 <body>
+
+
+
     <header>
         <h1>Wren</h1>
         <nav>
-            <a href="">LINK 1</a>
-            <a href="">LINK 2</a>
-            <a href="">LINK 3</a>
+            <?php
+            wp_nav_menu(
+                array(
+                    'menu' => 'primary',
+                    'container' => '',
+                    'theme_location' => 'primary',
+                    'items_wrap' => '<ul class="navbar">%3$s</ul>'
+                )
+            );
+            ?>
         </nav>
+        <hr>
 
 
     </header>
