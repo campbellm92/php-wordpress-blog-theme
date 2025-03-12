7842829de6ed1da7ca20876900aadc359992c4d3
Matthew Campbell <m.campbell92@gmail.com>
2025-03-12 14:50:23 +0100

    add initial logic for rendering latests posts on homepage

--- front-page.php
+++ front-page.php
@@ -7,22 +7,21 @@ get_header();
 
 <main>
     <div class="content-wrapper">
-        <?php
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post();
-                the_content();
-            }
-        }
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()):
+                the_post(); ?>
+                <div class="content-preview">
+                    <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="post-meta"> <?php the_date(); ?>, by <?php the_author(); ?></div>
+                    <div class="post-excerpt-content"><?php the_excerpt(); ?></div>
+                    <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
 
-        ?>
-
-
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 </main>
-</div>
 
-
-</div>
 
 <?php
 get_footer();
