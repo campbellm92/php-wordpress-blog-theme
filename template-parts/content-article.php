0005f2fe4dd20672f243937fa3db166ea1eb6749
Matthew Campbell <m.campbell92@gmail.com>
2025-03-12 14:51:03 +0100

    add post category

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -4,5 +4,9 @@
     <div class="post-author">by <?php the_author() ?></div>
     <div class="post-date"><?php the_date() ?></div>
     <div class="post-content"><?php the_content() ?></div>
+    <div class="post-category"><?php the_category() ?></div>
     <div class="post-tags"><?php the_tags() ?></div>
+
+
+
 </article>
