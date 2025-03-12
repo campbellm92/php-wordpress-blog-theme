ba6fb46d21274ab7ae2163db359158140179bfbd
Matthew Campbell <m.campbell92@gmail.com>
2025-03-12 14:06:57 +0100

    add logic for content articles

---
+++ template-parts/content-article.php
@@ -0,0 +1,8 @@
+<article>
+    <h2 class="post-title"><?php
+    the_title(); ?></h2>
+    <div class="post-author">by <?php the_author() ?></div>
+    <div class="post-date"><?php the_date() ?></div>
+    <div class="post-content"><?php the_content() ?></div>
+    <div class="post-tags"><?php the_tags() ?></div>
+</article>
