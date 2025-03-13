7425c8a8bf96b706a0da9e5c863b853e15342f0c
Matthew Campbell <m.campbell92@gmail.com>
2025-03-13 12:44:09 +0100

    add divs for btn and card

--- front-page.php
+++ front-page.php
@@ -10,11 +10,13 @@ get_header();
         <?php if (have_posts()): ?>
             <?php while (have_posts()):
                 the_post(); ?>
-                <div class="content-preview">
+                <div class="content-preview-card">
                     <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <div class="post-meta"> <?php the_date(); ?>, by <?php the_author(); ?></div>
+                    <div class="post-meta"> <?php echo get_the_date(); ?>, by <?php the_author(); ?></div>
                     <div class="post-excerpt-content"><?php the_excerpt(); ?></div>
-                    <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+                    <div class="btn-read-more"><a href="<?php the_permalink(); ?>" class="read-more">Read More</a></div>
+                    <!-- <hr class="preview-divider"> -->
+
 
                 </div>
             <?php endwhile; ?>
